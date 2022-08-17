c379671b18156e28023999a7eafa7c816e23e57b
Jin Jong Yoo <jjyoo03@gmail.com>
2022-08-16 23:20:22 -0400

    result display

--- app.py
+++ app.py
@@ -7,24 +7,37 @@ import model as m
 app = Flask(__name__)
 
 
-@app.route('/', methods = ['GET','POST'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    if request.method == "POST":
-        gender = request.form["Gender"]
-        age = request.form["Age"]
-        hypertension = request.form["Hypertension"]
-        heart = request.form["HeartDisease"]
-        married = request.form["Marriage"]
-        work = request.form["WorkType"]
-        residence = request.form["Residence"]
-        glucose = request.form["GlucoseLevel"]
-        bmi = request.form["BMI"]
-        smoking = request.form["Smoking"]
-        #stroke_predicted = []
-        stroke_predicted = m.stroke_prediction(age,hypertension,heart,married,residence,glucose,bmi,gender,work,smoking)
-        sp = stroke_predicted
 
-    return render_template('index.html', my_stroke = sp)
+    word = ''
+
+    if request.method == "POST":
+
+        gender = float(request.form["Gender"])
+        age = float(request.form["Age"])
+        hypertension = float(request.form["Hypertension"])
+        heart = float(request.form["HeartDisease"])
+        married = float(request.form["Marriage"])
+        work = float(request.form["WorkType"])
+        residence = float(request.form["Residence"])
+        glucose = float(request.form["GlucoseLevel"])
+        bmi = float(request.form["BMI"])
+        smoking = float(request.form["Smoking"])
+        #stroke_predicted = []
+
+        stroke_predicted = m.stroke_prediction(
+            age, hypertension, heart, married, residence, glucose, bmi, gender, work, smoking)
+
+        if stroke_predicted == 0:
+            word = '***You are likely to NOT get a stroke***'
+        elif stroke_predicted == 1:
+            word = '***You are likely to get a stroke***'
+
+        print(word)
+
+    return render_template('index.html', my_stroke=word)
+    #my_stroke = stroke_predicted
 
 # @app.route('/sub', methods = ['POST'])
 # def submit():
@@ -34,5 +47,6 @@ def index():
 #         hypertension = request.form["Hypertension"]
 #     return render_template("sub.html", g = gender, a = age)
 
-if __name__== "__main__":
+
+if __name__ == "__main__":
     app.run(debug=True)
